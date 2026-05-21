37ce050fce05f5cbc936bfb5d423ce7cae758ce5
amiko <amikochixladze1234@gmail.com>
2026-05-21 23:39:00 +0400

    completing navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'cars',
+    loadComponent: () => import('./pages/cars/cars').then((m) => m.Cars),
+    title: 'Cars',
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then((m) => m.About),
+    title: 'About',
+  },
+];
--- src/app/components/shared/navbar/navbar.ts
+++ src/app/components/shared/navbar/navbar.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, viewChild, viewChildren } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { Button } from '../../ui/button/button';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgOptimizedImage, RouterLink, Button],
+  imports: [NgOptimizedImage, RouterLink, RouterLinkActive, Button],
   templateUrl: './navbar.html',
 })
-export class Navbar {}
+export class Navbar {
+  router = inject(Router);
+
+  readonly navItems = viewChildren<ElementRef<HTMLElement>>('navItem');
+  readonly buttonElement = viewChild<Button, ElementRef>(Button, { read: ElementRef });
+
+  private animateNavbar(): void {
+    const elements = this.navItems().map((el) => el.nativeElement);
+
+    const tl = gsap.timeline();
+
+    tl.from(elements, {
+      y: -20,
+      opacity: 0,
+      duration: 0.4,
+      stagger: 0.15,
+    }).from(this.buttonElement()?.nativeElement, {
+      y: -20,
+      opacity: 0,
+      duration: 0.4,
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.animateNavbar();
+  }
+}
